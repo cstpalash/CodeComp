a2532c81cb6c8ff077bae91dcbd435ae414971ee
cstpalash <cstpalash@gmail.com>
2021-10-21 15:54:44 +0530

    menu items

---
+++ Controllers/ApiDocController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CodeComp.Controllers
+{
+    public class ApiDocController : Controller
+    {
+        private readonly ILogger<ApiDocController> _logger;
+
+        public ApiDocController(ILogger<ApiDocController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
