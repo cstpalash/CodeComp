1d3d9dc4c83e3edbf3955e8c8b68ec38a6d72eed
cstpalash <cstpalash@gmail.com>
2021-10-21 13:34:57 +0530

    rules and leaderboard

---
+++ Controllers/LeaderboardController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CodeComp.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        private readonly ILogger<LeaderboardController> _logger;
+
+        public LeaderboardController(ILogger<LeaderboardController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/RulesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CodeComp.Controllers
+{
+    public class RulesController : Controller
+    {
+        private readonly ILogger<RulesController> _logger;
+
+        public RulesController(ILogger<RulesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
